701ec2a2a79d8708f514cbd7d8ff7951634ed23a
shineby <shineby@gmail.com>
2018-09-25 18:16:22 +0900

    add sort.

---
+++ lunchbox/lunchbox2.cpp
@@ -0,0 +1,124 @@
+// Test Cases (1 <= C <= 50)
+// # of items (1 <= N <= 100)
+// volume (1 <= W <= 1000)
+
+#include <iostream>
+#include <string.h>
+#include <limits.h>
+#include <algorithm>
+
+using namespace std;
+
+class LunchBox {
+public:
+	int heatTime;
+	int eatTime;
+};
+
+typedef struct _totalEat {
+	int heatTime;
+	int eatTime;
+	int sum(void) {
+		return heatTime + eatTime;
+	}
+} TotalTime;
+
+
+bool compare(LunchBox a, LunchBox b){
+	if(a.eatTime > b.eatTime) 
+		return true;
+	return false;
+}
+
+class MW {
+public:
+	
+	//int heatedBoxMap[10001];
+	LunchBox boxList[10001];
+	int nLunchBox;
+	int nheatedBox;
+	TotalTime totalRun;
+	int _temp;
+
+
+	MW() {
+		nLunchBox = 0;
+		nheatedBox = 0;
+		_temp = 0;
+	}
+
+	void init(void) {
+		nLunchBox = 0;
+		nheatedBox = 0;
+		_temp = 0;
+		//memset(heatedBoxMap, 0, sizeof(heatedBoxMap));
+	}
+
+	void inputHeatTime() {
+		int _heatTime = 0;
+		for ( int i = 0 ; i < nLunchBox; i++) {
+			cin >> _heatTime;
+			boxList[i].heatTime = _heatTime;
+		}
+	}
+	void inputEatTime() {
+		int _eatTime = 0;
+		for ( int i = 0 ; i < nLunchBox; i++) {
+			cin >> _eatTime;
+			boxList[i].eatTime = _eatTime;
+		}
+	}
+	void printBoxList(void) {
+		for ( int i = 0; i < nLunchBox ; i++) {
+			cout << "[" << i << "] " <<  "HeatTime: " << boxList[i].heatTime << " EatTime: " << boxList[i].eatTime << endl;
+		} 
+	}
+
+	int doMW(int start = 0) {
+		//TODO
+		//do microwave
+		
+	}
+	void sortBox(void  ) {
+		//TODO 
+		//sort box with eat time or heat time	
+		//printBoxList();		
+		sort(boxList,boxList+nLunchBox, compare);
+		//printBoxList();
+
+	}
+
+	
+
+};
+
+
+
+
+
+
+//----------------------------------------------------------------
+int main(void) {
+	int testCases = 0;
+
+	MW mw;
+	cin >> testCases;
+	while (testCases--){
+		mw.init();
+		int _nLunchBox = 0;
+		cin >> _nLunchBox;
+		mw.nLunchBox = _nLunchBox;
+		mw.inputHeatTime();		
+		mw.inputEatTime();
+		mw.printBoxList();
+		mw.sortBox();
+		mw.printBoxList();
+		mw.doMW();
+		//cout << mw.totalRun.sum() << endl;
+		
+		
+
+		
+	}
+}
+
